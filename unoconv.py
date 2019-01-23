a28cab8ab04c3303cb8aaae666c8671c7717572c
gx <optimism1207@outlook.com>
2019-01-23 16:33:51 +0800

    bug修复，新增指定文件夹深度

--- unoconv.py
+++ unoconv.py
@@ -5,6 +5,8 @@ import sys,os,argparse
 #filename split
 def fileNameSplit(targetFile):
     fileArgs = targetFile.split(".")
+    if len(fileArgs) == 1:
+        fileArgs.append("placeHolder")
     return fileArgs
  
 #for convert and printing 
@@ -17,19 +19,22 @@ def convertCommon(neededFileName, fileType, outputPath, inputPath):
         print("%s convert failed!" % (neededFileName))
 
 #find target file and call convertCommon
-def findTargetFile(outputFilePath, inputFilePath):
+def findTargetFile(outputFilePath, inputFilePath, depth=1):
     if os.path.isfile(inputFilePath): #判断输入是不是文件
         filename = os.path.basename(inputFilePath)
         fileValue = fileNameSplit(filename)
-        if fileValue[1] in convertRelation and fileType == convertRelation[fileValue[1]]:      
+        if fileValue[1] in convertRelation and fileType == convertRelation.get(fileValue[1], 'nokey'):      
             convertCommon(filename, fileType, outputFilePath, inputFilePath) 
     else:
-        for root,dirs,filenames in os.walk(inputFilePath):
+        neededDepth = len(inputFilePath.split(os.path.sep)) + depth - 1
+        for dirpath,dirs,filenames in os.walk(inputFilePath):
             for filename in filenames:
-                fileValue = fileNameSplit(filename)
-                if fileValue[1] in convertRelation and fileType == convertRelation[fileValue[1]]: 
-                    inputFile = inputFilePath + filename
-                    convertCommon(filename, fileType, outputFilePath, inputFile)
+                currentDepth = len(os.path.join(dirpath, filename).split(os.path.sep))
+                if currentDepth <= neededDepth and not filename.startswith("."):
+                    fileValue = fileNameSplit(filename)
+                    if fileValue[1] in convertRelation and fileType == convertRelation.get(fileValue[1], 'nokey'): 
+                        inputFile = os.path.join(dirpath, filename)
+                        convertCommon(filename, fileType, outputFilePath, inputFile)
 
 os.system("unoconv --listener ")
 
@@ -40,18 +45,20 @@ parse = argparse.ArgumentParser(description="test!!")
 parse.add_argument("-f", help="filetype needed to convert")
 parse.add_argument("-i", help="path to inputfile")
 parse.add_argument("-o", help="path to outputfile")
+parse.add_argument("--depth", nargs='?', help="path depth, default=1")
 
 args = parse.parse_args()
 fileType = args.f
 inputFilePath = args.i
 outputFilePath = args.o
+depth = int(args.depth)
 
 if inputFilePath != None and outputFilePath != None: #输入输出路径不为空
     if not outputFilePath.endswith("/"): #输出路径没"/",自动加"/"
         outputFilePath = outputFilePath + "/"
     if os.path.isdir(inputFilePath) and (not inputFilePath.endswith("/")): #输入为路径并且没"/",自动加"/"
         inputFilePath = inputFilePath + "/"
-    findTargetFile(outputFilePath, inputFilePath)
+    findTargetFile(outputFilePath, inputFilePath, depth)
 else:
     print("Input and output file path needed.")
 
